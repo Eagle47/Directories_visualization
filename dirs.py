a2e9a141f173ccc4270738d12b21cd34a1ddbae9
Eagle47 <egor.vorontcov@mail.ru>
2019-11-18 18:38:06 +0300

    (Bad) visualisation 2 lvls of dirs

--- dirs.py
+++ dirs.py
@@ -18,7 +18,7 @@ def get_list_of_dirs(dir_name):
 def dir_size(dir_name):
     l = get_list_of_dirs(dir_name)
 
-    s = 0;
+    s = 0
     for x in l:
         if os.path.isfile(x):
             s += os.path.getsize(x)
@@ -36,17 +36,37 @@ l_dirs = get_list_of_dirs(top_dir)
 size_top_dir = dir_size(top_dir)
 
 l_size = []
+s = 0
 for x in l_dirs:
     if os.path.isdir(x):
-        tmp = dir_size(x)
-        l_size.append(tmp / size_top_dir)
-l_size.append((size_top_dir - sum(l_size)) / size_top_dir)
+        l_tmp = []
+        prev_dir_size = dir_size(x)
+        l_dirs_tmp = get_list_of_dirs(x)
+
+        for y in l_dirs_tmp:
+            if os.path.isdir(y):
+                tmp = dir_size(y)
+                l_tmp.append(tmp / prev_dir_size)
+
+        l_tmp.append((prev_dir_size - sum(l_tmp)) / prev_dir_size)
+        l_size.append(l_tmp)
+
+        s += dir_size(x)
+l_size.append([(size_top_dir - s) / size_top_dir])
 
 fig, ax = plt.subplots()
 
 size = 0.3
-vals = np.array(l_size)
+vals = []
+for x in l_size:
+    vals.append(sum(x))
+
+flattened = []
+for i in l_size:
+    for j in i:
+        flattened.append(j)
 
 ax.pie(vals, radius=1, wedgeprops=dict(width=size, edgecolor='w'))
+ax.pie(flattened, radius=1 + size, wedgeprops=dict(width=size, edgecolor='w'))
 
 plt.show()
